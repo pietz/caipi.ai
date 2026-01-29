95fdb12bf9709bf3569ca7905d9c7b150245bbd9
Paul-Louis Pröve <plp@workgenius.com>
2026-01-29 11:16:43 +0100

    Update cask to v0.1.9

--- Casks/caipi.rb
+++ Casks/caipi.rb
@@ -1,6 +1,6 @@
 cask "caipi" do
-  version "0.1.8"
-  sha256 "80e827bd09ec29f84e60a4bab5d967ab24ec86728db571f30b59d568e8106dd8"
+  version "0.1.9"
+  sha256 "ff4741ba7db280d5082685037d17a203354380afeba68bae96662d2046b81d5c"
 
   url "https://github.com/pietz/caipi.ai/releases/download/v#{version}/caipi_aarch64.dmg"
   name "Caipi"
